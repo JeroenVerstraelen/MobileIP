4a1cbb9a3b1905368a0839065bcdb8ca216b36d9
babbel056 <hermans.anthony@hotmail.com>
2017-10-29 14:38:55 +0100

    clean folder

---
+++ mobileip/structs/ICMP_Advertisement.hh
@@ -0,0 +1,12 @@
+// This file contains the struct for an ICMP advertisement message
+// (sent by an agent to let neigboring interfaces know it's presence)
+struct ICMPAdvertisement{
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+    uint8_t numAddrs;
+    uint8_t addrEntrySize;
+    uint16_t lifetime;
+    uint32_t routerAddress;
+    uint32_t preferenceLevel;
+}
